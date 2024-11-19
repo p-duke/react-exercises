08451b33fc6f570320545c063b6e6c0e184cfeeb
Peter Duke <peter.duke@wunderkind.co>
2024-11-19 09:23:12 -0500

    calculate winner

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,43 +5,95 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(null));
+    const emptyBoard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
+
+    const [squares, setSquares] = useState(emptyBoard);
     const [xIsNext, setXIsNext] = useState(true);
 
-    const handleClick = (i) => {
-        if (squares[i]) {
+    const handleClick = (r,c) => {
+        if (squares[r][c] || calculateWinner(squares)) {
             return;
         }
 
         const nextSquares = squares.slice();
 
         if (xIsNext) {
-            nextSquares[i] = 'X';
+            nextSquares[r][c] = 'X';
         } else {
-            nextSquares[i] = 'O';
+            nextSquares[r][c] = 'O';
         }
 
         setXIsNext(!xIsNext);
         setSquares(nextSquares);
     };
 
+    const calculateWinner = (board) => {
+        const rows = board.length;
+        const cols = board[0].length;
+        let winner = '';
+
+        // row major traversal
+        for (let row = 0; row < rows; row++) {
+            if (board[row][0] === 'X' && board[row][1] === 'X' && board[row][2] === 'X') {
+                winner = 'X';
+            }
+
+            if (board[row][0] === 'O' && board[row][1] === 'O' && board[row][2] === 'O') {
+                winner = 'O';
+            }
+        }
+
+        // col major traversal
+        for (let col = 0; col < cols; col++) {
+            if (board[0][col] === 'X' && board[1][col] === 'X' && board[2][col] === 'X') {
+                winner = 'X';
+            }
+
+            if (board[0][col] === 'O' && board[1][col] === 'O' && board[2][col] === 'O') {
+                winner = 'O';
+            }
+        }
+
+        // diag top-left-to-bottom
+        if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] == 'X') {
+            winner = 'X';
+        } else if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] == 'O') {
+            winner = 'O';
+        }
+
+        // diag top-right-to-bottom
+        if (board[0][2] === 'X' && board[1][1] === 'X' && board[0][2] == 'X') {
+            return 'X';
+        } else if (board[0][2] === 'O' && board[1][1] === 'O' && board[0][2] == 'O') {
+            winner = 'O';
+        }
+
+        return winner.length ? `Winner is ${winner}!` : '';
+    }
+
     return (
         <>
+            <p>Current Turn: {xIsNext ? 'X' : 'O'}</p>
             <div className="board-row">
-                <Square onSquareClick={() => handleClick(0)} value={squares[0]}/>
-                <Square onSquareClick={() => handleClick(1)} value={squares[1]}/>
-                <Square onSquareClick={() => handleClick(2)} value={squares[2]}/>
+                <Square onSquareClick={() => handleClick(0, 0)} value={squares[0][0]}/>
+                <Square onSquareClick={() => handleClick(0, 1)} value={squares[0][1]}/>
+                <Square onSquareClick={() => handleClick(0, 2)} value={squares[0][2]}/>
             </div>
             <div className="board-row">
-                <Square onSquareClick={() => handleClick(3)} value={squares[3]}/>
-                <Square onSquareClick={() => handleClick(4)} value={squares[4]}/>
-                <Square onSquareClick={() => handleClick(5)} value={squares[5]}/>
+                <Square onSquareClick={() => handleClick(1, 0)} value={squares[1][0]}/>
+                <Square onSquareClick={() => handleClick(1, 1)} value={squares[1][1]}/>
+                <Square onSquareClick={() => handleClick(1, 2)} value={squares[1][2]}/>
             </div>
             <div className="board-row">
-                <Square onSquareClick={() => handleClick(6)} value={squares[6]}/>
-                <Square onSquareClick={() => handleClick(7)} value={squares[7]}/>
-                <Square onSquareClick={() => handleClick(8)} value={squares[8]}/>
+                <Square onSquareClick={() => handleClick(2, 0)} value={squares[2][0]}/>
+                <Square onSquareClick={() => handleClick(2, 1)} value={squares[2][1]}/>
+                <Square onSquareClick={() => handleClick(2, 2)} value={squares[2][2]}/>
             </div>
+            <p>{ calculateWinner(squares).length ? calculateWinner(squares) : null }</p>
         </>
     ); 
 }
